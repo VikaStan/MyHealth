14d1f5dc32189888f65c0a5ddb3cd0a7255d9217
Vika <vikastan325@gmail.com>
2025-06-14 18:35:10 +0300

    git ignore

--- app/src/main/java/com/example/myhealth/domain/usecases/GetDayByDateUseCase.kt
+++ app/src/main/java/com/example/myhealth/domain/usecases/GetDayByDateUseCase.kt
@@ -4,7 +4,7 @@ import com.example.myhealth.domain.models.Day
 import com.example.myhealth.domain.repository.DayRepository
 import java.time.LocalDate
 
-class GetDayUseByDateCase(private val dayRepository: DayRepository) {
+class GetDayByDateUseCase(private val dayRepository: DayRepository) {
 
     fun execute(date: LocalDate): Day {
         return dayRepository.getDayByDate(date)
